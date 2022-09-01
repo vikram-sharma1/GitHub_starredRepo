8d02911e49fbcdc685783a7dd5c1a6a845bf9e1c
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-09-01 10:32:19 +0530

    github Graph

--- src/components/ModalData.jsx
+++ src/components/ModalData.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import GitHubCalendar from 'react-github-calendar';
+import { useEffect, useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+
 
 const ModalData = () => {
+    const [username, setUsername] = useState('grubersjoe');
 
     let NameofUser = JSON.parse(localStorage.getItem('NameOfUser'))
     console.log('NameofUser:', NameofUser)
@@ -11,6 +16,8 @@ const ModalData = () => {
         <div>
             <h4>User Activity Graph</h4>
             <img src={`https://activity-graph.herokuapp.com/graph?username=${NameofUser}&bg_color=0D1117&color=5BCDEC&line=5BCDEC&point=FFFFFF&hide_border=true`} alt="" width='95%'/>
+
+            <GitHubCalendar username="vikram-sharma1" />
         </div>
     </>
   )
